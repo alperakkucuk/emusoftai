2c61c3f82843b1f9bd851a2db793c4384296c998
alperakkucuk <64375264+alperakkucuk@users.noreply.github.com>
2024-08-03 22:51:25 +0300

    Update get-data.php

--- phpfiles/get-data.php
+++ phpfiles/get-data.php
@@ -1,6 +1,6 @@
 <?php
-if (file_exists('phpfiles/data.json')) {
-    $data = json_decode(file_get_contents('phpfiles/data.json'), true);
+if (file_exists('data.json')) {
+    $data = json_decode(file_get_contents('data.json'), true);
     echo "Temperature: " . $data['temperature'] . ", Humidity: " . $data['humidity'] . ", CihazID: " . $data['cihazid'];
 } else {
     echo "No data available";
