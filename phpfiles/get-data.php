612387b7ea17f49d3a32f2bc0c8645da66b76179
alperakkucuk <64375264+alperakkucuk@users.noreply.github.com>
2024-09-02 18:50:25 +0300

    Delete phpfiles directory

--- phpfiles/get-data.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (file_exists('data.json')) {
-    $data = json_decode(file_get_contents('data.json'), true);
-    echo "Temperature: " . $data['temperature'] . ", Humidity: " . $data['humidity'] . ", CihazID: " . $data['cihazid'];
-} else {
-    echo "No data available";
-}
-?>
